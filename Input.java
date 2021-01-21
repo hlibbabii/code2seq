f6f9e7530bb9757822fde2641fdaf11ec3598a3c
hlib <hlibbabii@gmail.com>
2021-01-21 12:24:33 +0100

    commit uncommitted

--- Input.java
+++ Input.java
@@ -1,3 +1,6 @@
-public String getName() {
-		return name;
-	}
+private AmazonKinesisClient makeNewKinesisClient() {
+        AmazonKinesisClient client = new AmazonKinesisClient(getKinesisCredsProvider(), getClientConfiguration());
+        LOG.info("Using " + getRegion().getName() + " region");
+        client.setRegion(getRegion());       
+        return client;
+    }
